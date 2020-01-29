cefcbadc83596dc2204eb8dc82ebbe0297491d5b
ivanveles <ivanveles2003@yahoo.com>
2020-01-29 12:33:33 +0100

    Updates

---
+++ precache-manifest.f6e87092ab0a05df4e2572b8e0b9b008.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f3f4a1a05180531f76dac4c7c741f9",
+    "url": "/project2/index.html"
+  },
+  {
+    "revision": "d303c3e4c8e333883f56",
+    "url": "/project2/static/css/main.e6a3cbed.chunk.css"
+  },
+  {
+    "revision": "41ad5755921b519ce5f6",
+    "url": "/project2/static/js/2.20e939bb.chunk.js"
+  },
+  {
+    "revision": "927c14ee76f6023826bb25d1469cf515",
+    "url": "/project2/static/js/2.20e939bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d303c3e4c8e333883f56",
+    "url": "/project2/static/js/main.7152b15b.chunk.js"
+  },
+  {
+    "revision": "cfbf28d0ec1269c74cff",
+    "url": "/project2/static/js/runtime-main.4d9b012b.js"
+  },
+  {
+    "revision": "614384fd9a40a2e4773b9e3e92ad4009",
+    "url": "/project2/static/media/Data Scientist Resume.614384fd.png"
+  },
+  {
+    "revision": "a5adb20a0cf24238bbf3bc63cf0c1320",
+    "url": "/project2/static/media/Digital Marketing Resume.a5adb20a.png"
+  },
+  {
+    "revision": "b6481c430ed9c55d96788d81b451aa5c",
+    "url": "/project2/static/media/Graphic Design Resume.b6481c43.png"
+  },
+  {
+    "revision": "ce623a4fa9bc6ff79e66ecb6f91fac3f",
+    "url": "/project2/static/media/Laika1.ce623a4f.png"
+  },
+  {
+    "revision": "79a2017a5ba34967a69af76354dc5d7e",
+    "url": "/project2/static/media/Laika2.79a2017a.png"
+  },
+  {
+    "revision": "7fe1b9a0c9883d996c2002b6972e301a",
+    "url": "/project2/static/media/Laika4.7fe1b9a0.png"
+  },
+  {
+    "revision": "4d0c10bd1673ac57234a13587b8214b5",
+    "url": "/project2/static/media/Laika5.4d0c10bd.png"
+  },
+  {
+    "revision": "7133e54f552748ef5b6dcb34baa13495",
+    "url": "/project2/static/media/Pop-up.7133e54f.png"
+  },
+  {
+    "revision": "481d7f7e28b9ddc2d5f9113bdeab5b2e",
+    "url": "/project2/static/media/Web Development Resume.481d7f7e.png"
+  },
+  {
+    "revision": "9c94b6bd1cef4c57ab44a899047d19c6",
+    "url": "/project2/static/media/background.9c94b6bd.png"
+  },
+  {
+    "revision": "45c6bf6ca1b5811f49fdd0f4844132b4",
+    "url": "/project2/static/media/bill1.45c6bf6c.png"
+  },
+  {
+    "revision": "f802fffcbbc182cd7bdf625a9ebba080",
+    "url": "/project2/static/media/bill2.f802fffc.png"
+  },
+  {
+    "revision": "c7cfc5ab7453bf1d5104cef5a1ce2454",
+    "url": "/project2/static/media/bill3.c7cfc5ab.png"
+  },
+  {
+    "revision": "63413da71d0c380601385cad5288b2a0",
+    "url": "/project2/static/media/bill4.63413da7.png"
+  },
+  {
+    "revision": "dc8769ce9b240fb5caaf127063d113bb",
+    "url": "/project2/static/media/firstPage.dc8769ce.png"
+  },
+  {
+    "revision": "68bf2bb744af5d13888eadf0ff45a244",
+    "url": "/project2/static/media/laika3.68bf2bb7.png"
+  }
+]);
